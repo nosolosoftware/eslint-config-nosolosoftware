daf5b176885551761795401f4f3a8fec2b1891dc
pacop <1923775+pacop@users.noreply.github.com>
2023-03-16 15:02:00 +0100

    Added prettier-conflict

---
+++ prettier-conflict.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    'indent': 'off',
+    'max-len': 'off',
+    'operator-linebreak': 'off',
+    'no-confusing-arrow': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
+    'brace-style': 'off',
+  }
+}
